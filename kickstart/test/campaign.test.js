bad706dc82bf82f6508972f286e4bd9d7e59b2f5
kamilkulikTeck <kulikkamil@gmail.com>
2021-09-19 23:17:09 +0200

    update test warmup beforeEach

--- kickstart/test/campaign.test.js
+++ kickstart/test/campaign.test.js
@@ -15,4 +15,17 @@ beforeEach(async () => {
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface)) // constructor accepts js obj
     .deploy({ data: compiledFactory.bytecode }) // pass in compiled ABI
     .send({ from: accounts[0], gas: '1000000' }); // deploy it and send transaction to the network
+
+  // let's create a Campaign from the get go here to have it available in every test
+  await factory.methods.createCampaign('100').send({
+    from: accounts[0],
+    gas: '1000000',
+  });
+
+  // using faactory to get deployed campaigns to access address of the above campaign contract
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+  // we now need a local JS representation of the contract
+  // the contract was already deployed so we just pass in the interface
+  // and the address of the contract
+  campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign.interface), campaignAddress);
 });
