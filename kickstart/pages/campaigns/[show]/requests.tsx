4441a4d3128bd8b5a49bd5ece900e1789cea3904
kamilkulikTeck <kulikkamil@gmail.com>
2021-09-25 20:20:38 +0200

    add functionality to fetch all pending requests on a contract

--- kickstart/pages/campaigns/[show]/requests.tsx
+++ kickstart/pages/campaigns/[show]/requests.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Layout from '../../../components/layout';
 import Link from 'next/link';
-import { Button } from 'semantic-ui-react';
-import { useRouter } from 'next/router';
+import { Button, Table } from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
 
-const RequestIndexPage = () => {
-  const router = useRouter();
-  const { show: address } = router.query;
+export async function getServerSideProps({ params: { show: address } }) {
+  const campaign = Campaign(address);
+  const requestCount = await campaign.methods.getRequestCount().call();
+  const requests = await Promise.all(
+    Array(requestCount)
+      .fill(0)
+      .map((element, index) => {
+        return campaign.methods.requests(index).call();
+      })
+  );
+  const requestsJson = JSON.stringify(requests);
+  return { props: { address, requestsJson, requestCount } };
+}
+
+const RequestIndexPage = ({ address, requestsJson, requestCount }) => {
+  // const router = useRouter();
+  // const { show: address } = router.query;
+
+  const { Body, HeaderCell, Header, Row } = Table;
 
   return (
     <Layout>
@@ -16,6 +32,17 @@ const RequestIndexPage = () => {
           <Button primary>Add Request</Button>
         </a>
       </Link>
+      <Table>
+        <Header>
+          <HeaderCell>ID</HeaderCell>
+          <HeaderCell>Description</HeaderCell>
+          <HeaderCell>Amount</HeaderCell>
+          <HeaderCell>Recipient</HeaderCell>
+          <HeaderCell>Approval Count</HeaderCell>
+          <HeaderCell>Approve</HeaderCell>
+          <HeaderCell>Finalize</HeaderCell>
+        </Header>
+      </Table>
     </Layout>
   );
 };
